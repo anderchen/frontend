b2b2d95459e43e4cb89d089910cfd8fc09fdb151
Anderson Chen <ander.chen00@gmail.com>
2019-02-07 02:14:05 -0200

    Configures the timeline.js file

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 
+import twitterLogo from '../twitter.svg';
+import './Timeline.css';
+
 export default class Timeline extends Component {
+  state = {
+    newTweet: "",
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ newTweet: event.target.value });
+  };
+
+  handelNewTweet = (event) => {
+
+  };
+  
   render() {
-    return <h1>Timeline</h1>
+    return (
+      <div className="timeline-wrapper">
+        <img height={24} src={twitterLogo} alt="GoTwitter"/>
+        <form>
+          <textarea 
+            value={this.state.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder="O que está acontecendo?" />
+        </form>
+      </div>
+    );
   }
 }
